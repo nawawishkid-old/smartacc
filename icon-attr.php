f89436e608496e982dfd0f95b91dc0b2ea41246a
Nawawish Samerpark <hydrokidz@gmail.com>
2017-04-04 18:44:12 +0700

    Add files via upload

---
+++ icon-attr.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("frame1-head.html");?>
+	<title>Icon Attribution | SMARTACC</title>
+	<style type="text/css">
+		header {min-height: 3em;}
+		main {
+			width: 100%;
+			height: 80%;
+			overflow-x: hidden;
+			background-color: #ccc;
+		}
+		.attr {
+			width: 100%;
+			height: 5em;
+    	border-bottom: 1px solid ghostwhite;
+		}
+		.attr > div {height: 100%;}
+		.img {
+			width: 30%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			float: left;
+			background-color: #999;
+		}
+		.img > img {
+			max-width: 90%;
+			max-height: 90%;
+		}
+		.txt {
+			width: 70%;
+			padding: .5em;
+			float: right;
+    	background-color: #666;
+    	color: white;
+		}
+		main a, footer a {
+			cursor: pointer;
+    	color: #FAE03C;
+		}
+		#optimize {
+			width: 100%;
+			height: 10%;
+			min-height: 3em;
+			display: flex;
+	    justify-content: center;
+	    align-items: center;
+	    padding: .5em;
+	    background-color: #666;
+	    color: white;
+    	border-top: 3px solid white;
+		}
+		#optimize span {text-align: center;}
+	</style>
+</head>
+<body>
+	<?php include("frame2-header.html");?>
+	<main>
+		<div class="attr">
+			<div class="img">
+				<img src="media/icon-menu@48px-fff.png" alt="icon-menu"/>
+			</div>
+			<div class="txt">
+				This icon made by <a href="http://www.flaticon.com/authors/chris-veigt" title="Chris Veigt">Chris Veigt</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+			</div>
+		</div>
+		<div class="attr">
+			<div class="img">
+				<img src="media/icon-add@48px-fff.png" alt="icon-add"/>
+			</div>
+			<div class="txt">
+				This icon made by <a href="http://www.flaticon.com/authors/hanan" title="Hanan">Hanan</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+			</div>
+		</div>
+		<div class="attr">
+			<div class="img">
+				<img src="media/icon-bin@48px-000.png" alt="icon-bin"/>
+			</div>
+			<div class="txt">
+				This icon made by <a href="http://www.flaticon.com/authors/puppets" title="Puppets">Puppets</a> from <a href="http://www.flaticon.com" title="Flaticon">Flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+			</div>
+		</div>
+		<div class="attr">
+			<div class="img">
+				<img src="media/icon-edit@48px-000.png" alt="icon-edit"/>
+			</div>
+			<div class="txt">
+				This icon made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">Flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+			</div>
+		</div>
+		<div class="attr">
+			<div class="img">
+				<img src="media/icon-prev@48px-fff.png" alt="icon-back"/>
+			</div>
+			<div class="txt">
+				This icon made by <a href="http://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="http://www.flaticon.com" title="Flaticon">Flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+			</div>
+		</div>
+		<div class="attr">
+			<div class="img">
+				<img src="media/preloader-snake@100px.gif"/>
+			</div>
+			<div class="txt">
+				This preloader made by <a href="https://preloaders.net/">Preloaders.net</a>
+			</div>
+		</div>
+	</main>
+	<footer id="optimize">
+		<span>
+			All of these PNG images are optimized by <a href="http://optimizilla.com/">Optimizilla</a>
+		</span>
+	</footer>
+</body>
+</html>
